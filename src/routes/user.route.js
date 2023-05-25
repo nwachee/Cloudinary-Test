457d24a0291a72a20ed2a4601916e0450de95ba7
Nwachee <fuzzy245.in@gmail.com>
2023-05-25 15:45:36 +0100

    Initial Setup

---
+++ src/routes/user.route.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const userController = require('../controllers/user.controller')
+const authenticate = require('../middlewares/auth.middleware')
+
+router.post('/', userController.createUser)
+router.get('/:id', userController.findUser)
+router.patch('/:id', userController.updateUser)
+router.get('/', authenticate, userController.findUsers)
+router.delete('/:id', userController.deleteUser)
+
+module.exports = router 
+

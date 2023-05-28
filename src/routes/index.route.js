c2025e8557aa2d9225c6ab1cb0c694ae65cf876a
Nwachee <fuzzy245.in@gmail.com>
2023-05-28 10:57:25 +0100

    Fixed up the Readme

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const fileRoute = require('./fileupload.route')
+const docRoute = require('./doc.route')
 
 router.use('/upload', fileRoute)
+router.use('/doc', docRoute)
 
 module.exports = router
